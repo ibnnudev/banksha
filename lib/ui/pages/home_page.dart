6651ea7b407f3282a8c80dde2d488160c457a05e
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2024-01-30 20:33:09 +0700

    Refactor home page layout and add profile section

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -6,75 +6,145 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: lightBackgroundColor,
-      bottomNavigationBar: BottomAppBar(
-        elevation: 0,
-        color: Colors.white,
-        shape: const CircularNotchedRectangle(),
-        clipBehavior: Clip.antiAlias,
-        notchMargin: 6,
-        padding: const EdgeInsets.all(0),
-        child: BottomNavigationBar(
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: lightBackgroundColor,
+        bottomNavigationBar: BottomAppBar(
           elevation: 0,
-          backgroundColor: Colors.white,
-          type: BottomNavigationBarType.fixed,
-          selectedItemColor: blueColor,
-          unselectedItemColor: blackColor,
-          showSelectedLabels: true,
-          showUnselectedLabels: true,
-          selectedLabelStyle: blueTextStyle.copyWith(
-            fontSize: 10,
-            fontWeight: medium,
+          color: Colors.white,
+          shape: const CircularNotchedRectangle(),
+          clipBehavior: Clip.antiAlias,
+          notchMargin: 6,
+          padding: const EdgeInsets.all(0),
+          child: BottomNavigationBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: blueColor,
+            unselectedItemColor: blackColor,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            selectedLabelStyle: blueTextStyle.copyWith(
+              fontSize: 10,
+              fontWeight: medium,
+            ),
+            unselectedLabelStyle: blackTextStyle.copyWith(
+              fontSize: 10,
+              fontWeight: medium,
+            ),
+            items: [
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_overview.png',
+                  width: 20,
+                  color: blueColor,
+                ),
+                label: 'Overview',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_history.png',
+                  width: 20,
+                ),
+                label: 'History',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_statistic.png',
+                  width: 20,
+                ),
+                label: 'Statistic',
+              ),
+              BottomNavigationBarItem(
+                icon: Image.asset(
+                  'assets/icon_reward.png',
+                  width: 20,
+                ),
+                label: 'Rewarn',
+              ),
+            ],
           ),
-          unselectedLabelStyle: blackTextStyle.copyWith(
-            fontSize: 10,
-            fontWeight: medium,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: purpleColor,
+          shape: const CircleBorder(),
+          child: Image.asset(
+            'assets/icon_plus_circle.png',
+            width: 24,
+            color: whiteColor,
           ),
-          items: [
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/icon_overview.png',
-                width: 20,
-                color: blueColor,
-              ),
-              label: 'Overview',
-            ),
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/icon_history.png',
-                width: 20,
-              ),
-              label: 'History',
-            ),
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/icon_statistic.png',
-                width: 20,
-              ),
-              label: 'Statistic',
-            ),
-            BottomNavigationBarItem(
-              icon: Image.asset(
-                'assets/icon_reward.png',
-                width: 20,
-              ),
-              label: 'Rewarn',
-            ),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        body: ListView(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+          ),
+          children: [
+            buildProfile(),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        backgroundColor: purpleColor,
-        shape: const CircleBorder(),
-        child: Image.asset(
-          'assets/icon_plus_circle.png',
-          width: 24,
-          color: whiteColor,
-        ),
+    );
+  }
+
+  Widget buildProfile() {
+    return Container(
+      margin: const EdgeInsets.only(
+        top: 40,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Howdy,',
+                style: greyTextStyle.copyWith(fontSize: 16),
+              ),
+              const SizedBox(
+                height: 2,
+              ),
+              Text(
+                'Shaynahan',
+                style: blackTextStyle.copyWith(
+                  fontSize: 20,
+                  fontWeight: semiBold,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: 60,
+            height: 60,
+            decoration: const BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: AssetImage('assets/img_profile.png'),
+              ),
+            ),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                width: 16,
+                height: 16,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: whiteColor,
+                ),
+                child: const Center(
+                  child: Icon(
+                    Icons.check_circle,
+                    color: greenColor,
+                    size: 14,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
